15c24fc808eae6373bad5b8e29afc1123ca999f0
theantonyis <kostanuka@gmail.com>
2025-05-22 16:12:10 +0300

    fixed frontend structure

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,27 +1,8 @@
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
-//
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-//
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-//
-// const eslintConfig = [...compat.extends("next/core-web-vitals")];
-//
-// eslintConfig.push({
-//   settings: {
-//     "import/resolver": {
-//       "alias": {
-//         "map": [
-//           ["@", "./"]
-//         ],
-//         "extensions": [".js", ".jsx", ".ts", ".tsx"]
-//       }
-//     }
-//   }
-// });
-//
-// export default eslintConfig;
+// eslint.config.mjs
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat();
+
+export default [
+  ...compat.extends("next/core-web-vitals")
+];
