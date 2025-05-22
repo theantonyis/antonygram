de4ff35b1f7be04bef77043bd25313bd5aadc2e1
theantonyis <kostanuka@gmail.com>
2025-05-22 17:03:44 +0300

    fixed frontend structure

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,15 +2,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/login',
-        permanent: false, // Use false for 307 Temporary Redirect, similar to your middleware
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: '/login',
+  //       permanent: false, // Use false for 307 Temporary Redirect, similar to your middleware
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
