defe8eb13706d494765661acfbbd3bfc3d217d7c
theantonyis <kostanuka@gmail.com>
2025-05-22 15:15:47 +0300

    changed origin for CORS

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,14 @@ import { initSocket } from './services/socket.js';
 
 dotenv.config();
 
+const allowedOrigins = [
+    'https://antonygram.vercel.app',
+    'http://localhost:3000'
+];
+
+
 const app = express();
-app.use(cors({origin: process.env.CLIENT_URL ||'*', credentials: true}));
+app.use(cors({origin: allowedOrigins, credentials: true}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
