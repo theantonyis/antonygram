48ba0c1e4e556022af64157e801519e704bef7a2
theantonyis <kostanuka@gmail.com>
2025-05-22 16:05:38 +0300

    fixed frontend structure

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,27 +1,27 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-eslintConfig.push({
-  settings: {
-    "import/resolver": {
-      "alias": {
-        "map": [
-          ["@", "./"]
-        ],
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
-      }
-    }
-  }
-});
-
-export default eslintConfig;
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+//
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+//
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+//
+// const eslintConfig = [...compat.extends("next/core-web-vitals")];
+//
+// eslintConfig.push({
+//   settings: {
+//     "import/resolver": {
+//       "alias": {
+//         "map": [
+//           ["@", "./"]
+//         ],
+//         "extensions": [".js", ".jsx", ".ts", ".tsx"]
+//       }
+//     }
+//   }
+// });
+//
+// export default eslintConfig;
